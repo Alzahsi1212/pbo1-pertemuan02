f9d7f8e9d04cb6de7419e25daabd2832e4db5d2a
Alzahsi1212 <56376458+Alzahsi1212@users.noreply.github.com>
2023-05-08 18:15:30 +0800

    Menambahkan perhitungan pada File serta Compile & Run

--- InputOutput.java
+++ InputOutput.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
+
 public class InputOutput{
-  public static void main(String [] args){
-    Scanner scanner = new Scanner(System.in);
-    String nama;
-    System.out.print("Masukkan nama kamu : ");
-    nama = scanner.nextLine();
-    System.out.println("Halo, "+nama);
+    public static void main(String [] args){
+        Scanner scanner = new Scanner(System.in);
+        String nama;
+        int jumlahsks;
+        double IPK;
+
+        System.out.print("Masukkan nama kamu : ");
+        nama = scanner.nextLine();
+        System.out.println("Halo, "+nama);
+
+        System.out.print("Masukkan jumlah SKS yang sudah ditempuh : ");
+        jumlahsks = scanner.nextInt();
+
+        System.out.print("Masukkan IPK sementara : ");
+        IPK = scanner.nextDouble();
+
+        double jumlah = IPK * jumlahsks;
+        System.out.print("Total Nilai = "+jumlah);
+    }
   }
-}
